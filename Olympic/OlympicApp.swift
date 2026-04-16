7141e567075a1b1a6abb84c834bd1a550c0b4837
LocalZombie1999 <r.annette.barrles08@gmail.com>
2026-04-16 12:45:41 -0700

    Initial Commit

---
+++ Olympic/OlympicApp.swift
@@ -0,0 +1,17 @@
+//
+//  OlympicApp.swift
+//  Olympic
+//
+//  Created by BARRALES, ROXY on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct OlympicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
